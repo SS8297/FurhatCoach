b8bb91f231f1eaa296ce616f4bc9d748c40a88ae
Shuai Shao <shuai1997@hotmail.se>
2023-12-04 22:35:31 +0100

    emotion detection

--- python/cam.py
+++ python/cam.py
@@ -2,6 +2,31 @@ import numpy as np
 import cv2 as cv
 from feat import Detector
 from IPython.display import Image
+from feat.utils import FEAT_EMOTION_COLUMNS
+
+
+detector = Detector(face_model="retinaface", au_model = "xgb", emotion_model="resmasknet")
+
+def proc_image(img, detector):
+
+    #img = cv.resize(img, dsize=(120, 180), interpolation=cv.INTER_CUBIC)
+    detected_faces = detector.detect_faces(img)
+    detected_landmarks = detector.detect_landmarks(frame, detected_faces)
+    detected_emotions = detector.detect_emotions(frame, detected_faces, detected_landmarks)
+
+    em = detected_emotions[0]
+    em_labels = em.argmax(axis=1)
+
+    for face, label in zip(detected_faces[0], em_labels):
+        (x0, y0, x1, y1, p) = face
+        img = cv.rectangle(img, (int(x0), int(y0)), (int(x1), int(y1)), color = (0, 0, 255), thickness = 3)
+        cv.putText(img, FEAT_EMOTION_COLUMNS[label], (int(x0)-10, int(y0)-10), fontFace = 0, color = (0, 0, 255), thickness = 2, fontScale = 1)
+        cv.imshow('frame', img)
+        print(f"\"patientState\" : \"{FEAT_EMOTION_COLUMNS[label]}\"")
+        #print(detected_landmarks)
+
+
+counter = 0
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
         print("Cannot open camera")
@@ -14,13 +39,15 @@ while True:
     if not ret:
         print("Can't receive frame (steam end?).Exiting ...")
         break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    if (counter == 0):
+        proc_image(frame, detector)
+    counter += 1
+    counter = counter % 8
     if cv.waitKey(1) == ord('q'):
          break
 
-#When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
+
